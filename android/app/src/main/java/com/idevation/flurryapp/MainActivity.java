d24890a72d12f4f333a8a99a33f388f4f4096a5c
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2021-10-20 12:38:16 +0500

    first commit

---
+++ android/app/src/main/java/com/idevation/flurryapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.idevation.flurryapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FlurryAnalytics, FlurryAnalyticsObject, FlurryAnalyticsOptions } from '@ionic-native/flurry-analytics/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  flu:FlurryAnalyticsObject;
+
+  constructor(
+    private flurryAnalytics: FlurryAnalytics
+  ) {}
+
+  ionViewDidEnter(){
+    this.initFlurry()
+  }
+
+  initFlurry(){
+    const options: FlurryAnalyticsOptions = {
+      appKey: 'RYZR22WBXS8XND47KHVQ', // REQUIRED
+      reportSessionsOnClose: true,
+      enableLogging: true
+     }
+     this.flu = this.flurryAnalytics.create(options);
+  }
+
+
+  logEvt(){
+    console.log('asdf');
+    debugger
+    this.flu.logEvent('testEvt')
+       .then(() => console.log('Logged an event!'))
+       .catch(e => console.log('Error logging the event', e));
+  }
 
 }
